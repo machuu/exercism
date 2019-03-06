b2bf4a4e99c44e369e7485754e543c3cdbc42ce3
Matt Hoover <matt@hoover.ml>
2019-03-06 05:55:43 -0500

    Complete Python Armstrong Numbers exercise

--- exercises/python/armstrong-numbers/armstrong_numbers.py
+++ exercises/python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,14 @@
 def is_armstrong(number):
-    pass
+    str_number = str(number)
+    num_digits = len(str_number)
+
+    armstrong_sum = 0
+
+    for str_digit in str_number:
+        digit = int(str_digit)
+        armstrong_sum += digit ** num_digits
+
+    if armstrong_sum == number:
+        return True
+    else:
+        return False
