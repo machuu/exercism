a968a5f5cd6b880d03c43a75fb21192ae4809d1b
Matt Hoover <matt@hoover.ml>
2019-04-09 06:53:33 -0400

    Rename symbols to match method name called by test

--- exercises/ruby/space-age/space_age.rb
+++ exercises/ruby/space-age/space_age.rb
@@ -2,14 +2,14 @@ class SpaceAge
   # Orbital periods in units of Earth Years
   SECONDS_PER_EARTH_YEAR = 31557600.0
   YEAR_LENGTH_ON_PLANET = {
-    mercury: 0.2408467,
-    venus:   0.61519726,
-    earth:   1.0,
-    mars:    1.8808158,
-    jupiter: 11.862615,
-    saturn:  29.447498,
-    uranus:  84.016846,
-    neptune: 164.79132,
+    on_mercury: 0.2408467,
+    on_venus:   0.61519726,
+    on_earth:   1.0,
+    on_mars:    1.8808158,
+    on_jupiter: 11.862615,
+    on_saturn:  29.447498,
+    on_uranus:  84.016846,
+    on_neptune: 164.79132,
   }
 
   def initialize(age_in_seconds)
@@ -18,34 +18,34 @@ class SpaceAge
   end
 
   def on_mercury
-    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:mercury]
+    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:on_mercury]
   end
 
   def on_venus
-    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:venus]
+    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:on_venus]
   end
 
   def on_earth
-    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:earth]
+    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:on_earth]
   end
 
   def on_mars
-    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:mars]
+    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:on_mars]
   end
 
   def on_jupiter
-    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:jupiter]
+    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:on_jupiter]
   end
 
   def on_saturn
-    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:saturn]
+    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:on_saturn]
   end
 
   def on_uranus
-    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:uranus]
+    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:on_uranus]
   end
 
   def on_neptune
-    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:neptune]
+    return @age_in_earth_years / YEAR_LENGTH_ON_PLANET[:on_neptune]
   end
 end
