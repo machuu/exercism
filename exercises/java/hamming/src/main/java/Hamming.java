1f2857c58670786b29889e199ce3f45a2e42c8ba
Matt Hoover <matt@hoover.ml>
2019-07-20 06:30:12 -0400

    java-hamming: Pass all tests

--- exercises/java/hamming/src/main/java/Hamming.java
+++ exercises/java/hamming/src/main/java/Hamming.java
@@ -1,13 +1,47 @@
 class Hamming {
 
+	private String leftStrand;
+	private int leftLength;
+
+	private String rightStrand;
+	private int rightLength;
+
+	public int hammingDistance;
+
     Hamming(String leftStrand, String rightStrand) {
-		int leftLength = leftStrand.length();
-		int rightLength = rightStrand.length();
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		this.leftStrand = leftStrand;
+		this.rightStrand = rightStrand;
+
+		this.leftLength = leftStrand.length();
+		this.rightLength = rightStrand.length();
+
+		if ( leftLength == rightLength ) {
+			getHammingDistance();
+		} else {
+			if ( leftStrand.isEmpty() ) {
+        		throw new IllegalArgumentException("left strand must not be empty.");
+			} else if ( rightStrand.isEmpty() ) {
+        		throw new IllegalArgumentException("right strand must not be empty.");
+			} else {
+				throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+			}
+		}
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		hammingDistance = 0;
+
+		// Test character by character and count mismatches
+		for ( int i = 0; i < leftLength; i++ ) {
+			int leftChar  = (int)leftStrand.charAt(i);
+			int rightChar = (int)rightStrand.charAt(i);
+
+			if ( leftChar != rightChar ) {
+				hammingDistance++;
+			}
+		}
+
+		return hammingDistance;
     }
 
 }
