5386cc7b6479c6c79602b6ca6c5bcfb98996ed69
Matt Hoover <matt@hoover.ml>
2019-07-30 09:28:01 -0400

    java-pig-latin: Add logic for multi-character consonant sounds

--- exercises/java/pig-latin/src/main/java/PigLatinTranslator.java
+++ exercises/java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -12,25 +12,48 @@ class PigLatinTranslator {
 		"xr",
 		"yt"
 	};
+	private String[] beginMultiCharConsonantSounds = new String[] {
+		"ch",
+		"qu",
+		"th",
+		"thr"
+	};
 
 	String translate(String word) {
 		
 		String startsWith = "";
+		Integer startSoundLength = 1;
 		String translatedWord = "";
 
 		// Check if word starts with a vowel sound
 		for ( String vowelSound : beginVowelSounds ) {
 			if ( word.startsWith(vowelSound) ) {
 				startsWith = "vowelSound";
+				startSoundLength = 0;
 			}
 		}
 
-		if (startsWith == "vowelSound" ) {
+		// Check if word starts with multi-character consonant
+		for ( String multiCharConsonant : beginMultiCharConsonantSounds ) {
+			if ( word.startsWith(multiCharConsonant) ) {
+				startsWith = "multiCharConsonant";
+				startSoundLength = multiCharConsonant.length();
+			} else if ( word.startsWith(multiCharConsonant,1) ) {
+				startsWith = "multiCharConsonant";
+				startSoundLength = multiCharConsonant.length() + 1;
+			}
+		}
+
+		if ( startsWith == "vowelSound" ) {
 			// translate word starting with vowel sound
 			// just append 'ay' to word
 			translatedWord = word + "ay";
+		} else if ( startsWith == "multiCharConsonant" ) {
+			// translate word starting with multi-character consonant
+			// move length of consonant sound to end of word, then append 'ay'
+			translatedWord = word.substring(startSoundLength) + word.substring(0,startSoundLength) + "ay";
 		} else {
-			// translate word not starting with vowel sound
+			// translate word with single-character consonant
 			// move first letter to end, then append 'ay'
 			translatedWord = word.substring(1) + word.charAt(0) + "ay";
 		}
