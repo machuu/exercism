cdf91379165cf14b444cb0c21efb9182b6ee2e8b
Matt Hoover <matt@hoover.ml>
2019-07-31 06:25:41 -0400

    java-anagram: boilerplate and pass first test

---
+++ exercises/java/anagram/src/main/java/Anagram.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Anagram {
+
+	private String originalString = "";
+	private String sortedMatch = "";
+
+	Anagram(String baseString) {
+		originalString = baseString;
+	}
+
+	public List<String> match( List<String> matches ) {
+		List<String> emptyList = new ArrayList<String>(); 
+		return emptyList;
+	}
+
+}
