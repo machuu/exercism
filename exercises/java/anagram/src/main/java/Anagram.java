333ec82fa3f3f57f6eec83422101e77a28f1d344
Matt Hoover <matt@hoover.ml>
2019-07-31 08:33:20 -0400

    java-anagram: add logic to compare matches to base

--- exercises/java/anagram/src/main/java/Anagram.java
+++ exercises/java/anagram/src/main/java/Anagram.java
@@ -6,15 +6,32 @@ import java.util.List;
 class Anagram {
 
 	private String originalString = "";
-	private String sortedMatch = "";
+	private String sortedBase = "";
 
 	Anagram(String baseString) {
 		originalString = baseString;
+
+		// Sort characters from originalString in alphabetical order
+		//
+		sortedBase = sortStringInAlphaOrder( baseString );
+	}
+
+	private String sortStringInAlphaOrder( String stringToSort ) {
+		return String( Arrays.sort( inputString.toLowerCase().toCharArray() ) );
 	}
 
 	public List<String> match( List<String> matches ) {
-		List<String> emptyList = new ArrayList<String>(); 
-		return emptyList;
+		List<String> matchesList = new ArrayList<String>(); 
+		String sortedMatch = "" ;
+
+		for ( matchString : matches[] ) {
+			sortedMatch = sortStringInAlphaOrder(matchString);
+			if ( sortedMatch.equals(sortedBase) ) {
+				matchesList.add(matchString);
+			}
+		}
+
+		return matchesList;
 	}
 
 }
