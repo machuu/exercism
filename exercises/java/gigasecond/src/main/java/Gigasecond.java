36a1e2f22db08bf30fe0d7810fe19708bb6cff0a
Matt Hoover <matt@hoover.ml>
2019-08-20 05:50:50 -0400

    java-gigasecond: Use `Duration` class to store one gigasecond
    Per Mentor Suggestion:
      - store one gigasecond in a Duration object, and use the dedicated
      LocalDateTime method to add a Duration object
    
    the Duration object has context of its units, so the units don't need
    to be specified when adding to a LocalDateTime object.

--- exercises/java/gigasecond/src/main/java/Gigasecond.java
+++ exercises/java/gigasecond/src/main/java/Gigasecond.java
@@ -1,17 +1,18 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Duration;
 
 class Gigasecond {
 
-	private static final long ONE_GIGASECOND = 1_000_000_000;
+	private static final Duration ONE_GIGASECOND = Duration.ofSeconds(1_000_000_000);
 	private LocalDateTime gigasecondDateTime;
 
     Gigasecond(LocalDate moment) {
-		gigasecondDateTime = moment.atStartOfDay().plusSeconds(ONE_GIGASECOND);
+		gigasecondDateTime = moment.atStartOfDay().plus(ONE_GIGASECOND);
     }
 
     Gigasecond(LocalDateTime moment) {
-		gigasecondDateTime = moment.plusSeconds(ONE_GIGASECOND);
+		gigasecondDateTime = moment.plus(ONE_GIGASECOND);
     }
 
     LocalDateTime getDateTime() {
