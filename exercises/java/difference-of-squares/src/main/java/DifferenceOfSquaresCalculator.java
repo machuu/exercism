1cff685e1856bdeb63096794fabbd22c6580fb08
Matt Hoover <matt@hoover.ml>
2019-10-02 05:58:57 -0400

    `java-difference-of-squares`: Implement `computeSquareOfSumTo()`, and Pass first test
    Populate `computeSquareOfSumTo()`
    
    Add Helpers:
    
    - `computeSumTo()`
    - `computeSquareOf()`

--- exercises/java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ exercises/java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,7 +1,19 @@
 class DifferenceOfSquaresCalculator {
 
+    private int computeSumTo(int input) {
+        int sum = 0;
+        for ( int i = 1 ; i <= input ; i++ ) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    private int computeSquareOf(int input) {
+        return input * input;
+    }
+
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return computeSquareOf( computeSumTo(input) );
     }
 
     int computeSumOfSquaresTo(int input) {
