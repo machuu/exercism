f49849a05f3ebe3906277ec71515a778081f994f
Matt Hoover <matt@hoover.ml>
2019-10-05 06:27:53 -0400

    `ts-matrix`: Implement `Matrix`, and Pass first test
    Had to initialize `Matrix.columns`, since `yarn test` was
    producing errors for `xit` tests that referenced `.columns[x]`.

--- exercises/typescript/matrix/matrix.ts
+++ exercises/typescript/matrix/matrix.ts
@@ -1,7 +1,19 @@
 class Matrix {
-    constructor( /* Parameters go here */ ) {
-        // Your code here
+
+  public rows:    number[][] = [];
+  public columns: number[][] = [];
+
+  constructor( matrix_string: string ) {
+
+    // Iterate over string split on new lines
+    for ( let string_array_row of matrix_string.split("\n") ) {
+      // Add row to 2D rows array
+      //   - Split row string into an Array,
+      //   - Map String Array -> Number Array
+      //   - Push Number Array onto rows[n]
+      this.rows.push( string_array_row.split(" ").map( (str_number: string): number => Number(str_number) ) );
     }
+  }
 }
 
 export default Matrix
