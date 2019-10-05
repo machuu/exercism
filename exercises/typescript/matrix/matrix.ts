7ec19478a48e47819336a719447cdea692ba3c85
Matt Hoover <matt@hoover.ml>
2019-10-05 07:14:34 -0400

    `ts-matrix`: Implement transpose of `rows` into `columns`, all tests pass

--- exercises/typescript/matrix/matrix.ts
+++ exercises/typescript/matrix/matrix.ts
@@ -13,6 +13,21 @@ class Matrix {
       //   - Push Number Array onto rows[n]
       this.rows.push( string_array_row.split(" ").map( (str_number: string): number => Number(str_number) ) );
     }
+    this.transpose_rows_into_columns();
+  }
+
+  private transpose_rows_into_columns() {
+    // double loop
+    // This should work for rectangular matrices.
+    // iterate over column numbers in first row of array
+    for ( var col_index: number = 0; col_index < this.rows[0].length ; col_index++ ) {
+      // iterate over row numbers in array
+      this.columns[col_index] = [];
+      for ( var row_index: number = 0; row_index < this.rows.length ; row_index++ ) {
+        // switch col_index <-> row_index between rows[][] and columns[][]
+        this.columns[col_index][row_index] = this.rows[row_index][col_index];
+      }
+    }
   }
 }
 
