3706d84f4639896e174fa04a012d91b9eb5f5f96
Matt Hoover <matt@hoover.ml>
2020-01-20 20:42:04 -0500

    c-armstrong-numbers: implement first pass at armstrong numbers algorithm

--- exercises/c/armstrong-numbers/src/armstrong_numbers.c
+++ exercises/c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,5 +1,21 @@
 #include "armstrong_numbers.h"
 
 int is_armstrong_number(int candidate) {
-	return 1;
+	int digits = candidate;
+	int sum    = 0;
+	int num_digits = 0;
+
+	while ( digits != 0 ) {
+		// need to get length of candidate for exponent
+		sum += digits % 10;
+		digits /= 10;
+	}
+
+	if ( sum == candidate ) {
+		// Return true
+		return 1;
+	} else {
+		// Return false
+		return 0;
+	}
 }
