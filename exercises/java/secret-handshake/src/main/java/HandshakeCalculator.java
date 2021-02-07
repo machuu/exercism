8cb11998396debf9bf663569b7f78c3160409bb2
Matt Hoover <matt@hoover.ml>
2021-02-07 07:29:07 -0500

    java-secret-handshake: Implement `HandshakeCalculator.calculateHandshake()`

--- exercises/java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ exercises/java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,33 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Signal> handshake = new ArrayList<Signal>();
+
+        if ( (number & 0b00001) > 0 ) {
+            handshake.add(Signal.WINK);
+        }
+
+        if ( (number & 0b00010) > 0 ) {
+            handshake.add(Signal.DOUBLE_BLINK);
+        }
+
+        if ( (number & 0b00100) > 0 ) {
+            handshake.add(Signal.CLOSE_YOUR_EYES);
+        }
+
+        if ( (number & 0b01000) > 0 ) {
+            handshake.add(Signal.JUMP);
+        }
+
+        if ( (number & 0b10000) > 0 ) {
+            Collections.reverse(handshake);
+        }
+
+        return handshake;
     }
 
 }
