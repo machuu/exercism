3e5b92ec7bc798902829aa7bacb3a73b1af9d2ac
mathoove <mathoove@cisco.com>
2021-02-18 06:54:25 -0600

    java-proverb: Implement Proverb Constructor and `recite()`

--- exercises/java/proverb/src/main/java/Proverb.java
+++ exercises/java/proverb/src/main/java/Proverb.java
@@ -1,11 +1,26 @@
 class Proverb {
 
+    private String proverbLines = "";
+
     Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // Quit early if there are no words
+        if ( words.length == 0 ) {
+            return;
+        }
+
+        // Only add 'For want of' lines, when there are more than two words
+        if ( words.length >= 2 ) {
+            for ( int i = 0; i < words.length - 1; i++ ) {
+                proverbLines+="For want of a " + words[i] + " the " + words[i+1] + " was lost.\n";
+            }
+        }
+
+        // Add last line
+        proverbLines+="And all for the want of a " + words[0] + ".";
     }
 
     String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return proverbLines;
     }
 
 }
