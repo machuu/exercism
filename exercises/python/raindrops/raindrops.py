07d6679ab3457091bf99af4a6aea90ec12816a09
Matt Hoover <matt@hoover.ml>
2021-03-27 09:26:29 -0400

    python-raindrops: first pass at implementing `convert()`

--- exercises/python/raindrops/raindrops.py
+++ exercises/python/raindrops/raindrops.py
@@ -1,2 +1,20 @@
+from enum import Enum
+
+class Drop(Enum):
+  PLING = 3
+  PLANG = 5
+  PLONG = 7
+
+  def isIn(self, dropNumber):
+    return self.value % dropNumber == 0
+
 def convert(number):
-    pass
+  drops = ""
+  for drop in Drop:
+    if drop.isIn(number):
+      drops+=drop.name
+
+  if drops == "":
+    drops = str(number)
+
+  return drops
