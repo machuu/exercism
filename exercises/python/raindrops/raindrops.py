826acdcb282f7481e0ba12c183f860fd5bdb00b7
Matt Hoover <matt@hoover.ml>
2021-03-27 10:45:43 -0400

    python-raindrops: Rename method `isIn()` -> `isFactorOf`

--- exercises/python/raindrops/raindrops.py
+++ exercises/python/raindrops/raindrops.py
@@ -5,14 +5,14 @@ class Drop(Enum):
   Plang = 5
   Plong = 7
 
-  def isIn(self, dropNumber):
+  def isFactorOf(self, dropNumber):
     return (dropNumber % self.value == 0)
 
 def convert(number):
   drops = ""
 
   for drop in Drop:
-    if drop.isIn(number):
+    if drop.isFactorOf(number):
       drops+=drop.name
 
   # If no drops were added, assign the number
