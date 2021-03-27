1f221ec04cdc91fb9a413e7e0c8117b3c6733ce9
Matt Hoover <matt@hoover.ml>
2021-03-27 10:41:46 -0400

    python-raindrops: Refactor name of enum items

--- exercises/python/raindrops/raindrops.py
+++ exercises/python/raindrops/raindrops.py
@@ -1,18 +1,19 @@
 from enum import Enum
 
 class Drop(Enum):
-  PLING = 3
-  PLANG = 5
-  PLONG = 7
+  Pling = 3
+  Plang = 5
+  Plong = 7
 
   def isIn(self, dropNumber):
     return (dropNumber % self.value == 0)
 
 def convert(number):
   drops = ""
+
   for drop in Drop:
     if drop.isIn(number):
-      drops+=drop.name.capitalize()
+      drops+=drop.name
 
   if drops == "":
     drops = str(number)
