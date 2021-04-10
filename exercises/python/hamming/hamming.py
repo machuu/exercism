8988a16e5078a599bafa18a17fb141972dbf48db
Matt Hoover <matt@hoover.ml>
2021-04-10 05:53:36 -0400

    python-hamming: Implement `distance()`

--- exercises/python/hamming/hamming.py
+++ exercises/python/hamming/hamming.py
@@ -1,2 +1,13 @@
 def distance(strand_a, strand_b):
-    pass
+  if ( len(strand_a) != len(strand_b) ):
+    raise ValueError("Strands are not equal length")
+
+  if ( strand_a == strand_b ):
+    return 0
+
+  distance = 0
+  for char_index in range(0,len(strand_a)):
+    if (strand_a[char_index] != strand_b[char_index] ):
+      distance+=1
+
+  return distance
